13d871b367fff62a051fe96529c5684fbf24d67d
Zain <zain.9496@gmail.com>
2024-01-04 14:59:39 +0500

    Added robot base pose printing to the python sim code.

--- main.py
+++ main.py
@@ -19,6 +19,19 @@ p.setRealTimeSimulation(0)  # we want to be faster than real time :
 robot = Robot("urdf/mini_cheetah/mini_cheetah.urdf.xacro", [1,0,0.5], [0,0,0])
 
 while True:
+	base_pose = p.getBasePositionAndOrientation(robot.robot) # returns base position [x, y, z] and angular pose in quat [wx, wy, wz]
+
+	get_velocity = p.getBaseVelocity(robot.robot) # returns linear velocity [x, y, z] and angular velocity [wx, wy, wz]
+	get_invert = p.invertTransform(base_pose[0], base_pose[1]) 
+	get_matrix = p.getMatrixFromQuaternion(get_invert[1])
+
+	print(base_pose)
+	print(get_invert)
+	print(get_matrix)
+	print()
+	# print(get_velocity)
+
+
 	p.stepSimulation()
 
 	time.sleep(1 / 500)

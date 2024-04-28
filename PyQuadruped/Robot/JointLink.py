17c4f0a7b47903e8c3b52649f8f33aac6ba277ac
Zain <zain.9496@gmail.com>
2024-04-29 04:32:34 +0500

    Populated JointLink with some code

--- PyQuadruped/Robot/JointLink.py
+++ PyQuadruped/Robot/JointLink.py
@@ -4,19 +4,28 @@ from PyQuadruped.Spatial import ForceCrossProduct, SpatialTransformation
 from PyQuadruped.utils import GetRotMat, GetJointSubspace
 
 class JointLink:
-	def __init__(self, inertia, T, name = ""):
+	def __init__(self, inertia, T, joint_axis, parent, child, name = ""):
 		self.inertia = inertia
 		self.articulated_inertia = inertia.copy()
 		self.local_T = T
+		self.joint_axis = joint_axis
 		self.name = name
-		self.parent_joint = None 
+		
+		self.parent = None 
+		self.children = []
 		self.global_T = np.eye(6)
-		self.v = np.eye(6)
-		self.pa = np.zeros((6, 1))
 		self.parent_T_child = None
 
-		self.childred_joints = []
+		self.v = np.eye(6)
+		self.pa = np.zeros((6, 1))
 		self.U  = None
+
+	def AddChild(self, child):
+		self.children.append(child)
+
+	def Update(self, T):
+		pass
+
 
 
 	def __repr__(self):
